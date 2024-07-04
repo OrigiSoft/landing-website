b417d9229905a51bb87b10378643bff00a24beed
Xavier Foucron <73240020+xfoucron@users.noreply.github.com>
2024-07-04 22:43:58 +0200

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx,vue}'], // add your preferred file extensions here
+  theme: {
+    extend: {
+      colors: {
+        'background': '#FBF6F6',
+        'text': '#080706',
+        'primary': '#CB5242',
+        'secondary': '#ef8c7f',
+        'accent': '#FE5742'
+      },
+    },
+  },
+  plugins: [],
+};
